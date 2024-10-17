b928b41a40390b93290ab1500a0a7163d10f5c2a
Simonbunse <Simon.hastrup.jensen@gmail.com>
2024-10-17 14:55:13 +0200

    blablabla

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: '#274e13',
+        highlight: '#d9ead3',
+        bgGray: '#fbfafd',
       },
     },
   },
